03f158dcc5cb8baaf32fdea88078922a0dee4240
Dobi Okrasa <dobi.okrasa@gmail.com>
2018-08-03 13:01:20 +0200

    Mobile test 4: nav links

--- scripts/script.js
+++ scripts/script.js
@@ -6,17 +6,23 @@
           docToHideAndShow = $('nav, button, header, section, address'),
           navLinks = $('nav a'),
           doc = $('html, body'),
+          sectionPortfolio = $('#portfolio-0'),
           linkToPortfolio = $('#about a');
 
     function showMenu() {
       docToHideAndShow.fadeToggle(700, 'linear');
     }
 
+    function scrollFlow(section, margin) {
+      margin = margin || 0;
+      doc.animate({
+        scrollTop: section.offset().top - margin
+        }, 700, 'linear');
+    }
+
     linkToPortfolio.click(function() {
       c = $(this).data('page-number');
-      doc.animate({
-        scrollTop: $('#portfolio-0').offset().top
-        }, 700, 'linear');
+      scrollFlow(sectionPortfolio);
     });
 
     menuButton.click(function() {
@@ -25,19 +31,32 @@
     });
 
 
-    // SCROLL FIX FOR TOUCH DEVICES
+    // Scroll fix for touch devices
     function isMobile() {
       try { document.createEvent("TouchEvent"); return true; }
       catch(e) { return false; }
     }
 
-    let touchDevice = isMobile();
+    let touchDevice = isMobile(),
+        winH = $(window).height(),
+        // navLinkToAbout = $('#nav-link-to-About'),
+        // navLinkToPortfolio = $('#nav-link-to-Portfolio'),
+        sectionAbout = $('#about'),
+        heightSectionAbout = 700,
+        heightSectionPortfolio = 550;
 
     navLinks.click(function() {
       c = $(this).data('page-number');
-
+      let id = $(this).attr('id');
       if (touchDevice) {
-        c--;
+        switch (id) {
+          case 'nav-link-to-About':
+            scrollFlow(sectionAbout, (winH - heightSectionAbout));
+            break;
+          case 'nav-link-to-Portfolio':
+            scrollFlow(sectionPortfolio, (winH - heightSectionPortfolio));
+            break;
+        }
       }
 
       showMenu();
